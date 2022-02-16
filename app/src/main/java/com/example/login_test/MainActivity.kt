4701c52d646e1967cdd6d6de70d9c5a77d5f2512
amirmansouri <amir27069149@gmail.com>
2022-02-16 23:26:33 +0100

    login

---
+++ app/src/main/java/com/example/login_test/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.example.login_test
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
+
+class MainActivity : AppCompatActivity() {
+    val mAuth = FirebaseAuth.getInstance()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+var button = findViewById<Button>(R.id.button)
+
+        button.setOnClickListener{
+login()
+        }
+
+    }
+    private fun login () {
+        val emailTxt = findViewById<View>(R.id.emailTxt) as EditText
+        var email = emailTxt.text.toString()
+        val passwordTxt = findViewById<View>(R.id.passwordTxt) as EditText
+        var password = passwordTxt.text.toString()
+
+        if (!email.isEmpty() && !password.isEmpty()) {
+            this.mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener ( this, OnCompleteListener<AuthResult> { task ->
+                if (task.isSuccessful) {
+               //     startActivity(Intent(this, Timeline::class.java))
+                    Toast.makeText(this, "Successfully Logged in :)", Toast.LENGTH_LONG).show()
+                } else {
+                    Toast.makeText(this, "Error Logging in :(", Toast.LENGTH_SHORT).show()
+                }
+            })
+
+        }else {
+            Toast.makeText(this, "Please fill up the Credentials :|", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
